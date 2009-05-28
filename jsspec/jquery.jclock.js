7424d306c059d054bdf9bf5d31b86892175667a3
dsparling <scriptrunner.com@gmail.com>
2009-05-27 21:31:43 -0500

    added initial jsspec tests

--- jsspec/jquery.jclock.js
+++ jsspec/jquery.jclock.js
@@ -1,5 +1,5 @@
 /*
-* jQuery jclock - Clock plugin - v 2.2.1
+* jQuery jclock - Clock plugin - v 2.2.0
 * http://plugins.jquery.com/project/jclock
 *
 * Copyright (c) 2007-2009 Doug Sparling <http://www.dougsparling.com>
@@ -9,7 +9,7 @@
 (function($) {
  
   $.fn.jclock = function(options) {
-    var version = '2.2.1';
+    var version = '2.2.0';
  
     // options
     var opts = $.extend({}, $.fn.jclock.defaults, options);
